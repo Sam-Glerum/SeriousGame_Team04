d1480e04ef3c4233a29e68362276737e2f09bf53
Sam <samuelglerum@hotmail.com>
2021-06-14 16:21:10 +0200

    Finished loading level functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player
 {
     public int currentLevel = 0;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 }
--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static SaveSystem;
+using static Player;
 
 public class StartMenu : MonoBehaviour
 {
-
+    public Player player = new Player();
 
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
+        // Checks if a save file exists
+        if (SaveSystem.LoadData(player) != null)
+        {
+            if (player.currentLevel > 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + player.currentLevel);
+            }
+        }
+        else
+        {
+            // Creates a new save file if it does not yet exist
+            SaveSystem.SaveData(player);
+            // Loads the first level in the build settings list (after the menu scene)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
     public void LoadScene(string sceneName)
