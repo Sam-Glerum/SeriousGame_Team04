e023d2e699b28684a9fced1756bec329db07a3de
Marco <mnaf.weterings@gmail.com>
2021-06-14 19:15:14 +0200

    Added UIManagement for Audio and Text
    This can be used in the entire game

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,58 +6,51 @@ using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField]
+    public List<UIObject> objects;
+
     public Text UIText;
     public GameObject UI;
     public GameObject Game;
-    public AudioSource halloIkBenAlbert;
-    public AudioSource theater;
-    public AudioSource casette;
-    private bool firstTimeTheatre;
-    private bool firstTimeCasette;
 
     // Start is called before the first frame update
+
     void Start()
     {
         Game.SetActive(false);
-        firstTimeTheatre = true;
-        firstTimeCasette = true;
-        halloIkBenAlbert.Play();
+        StartCoroutine(example(
+            objects,
+        updateText,
+        onDone));
     }
 
     // Update is called once per frame
     void Update()
     {
-        ChangeUI();
+        
     }
 
-    public void ChangeUI()
-    {
-        if (!halloIkBenAlbert.isPlaying)
+    void updateText(UIObject audioFragment) 
         {
-            if (!theater.isPlaying && firstTimeTheatre)
-            {
-                firstTimeTheatre = false;
-                theater.Play();
-                UIText.fontSize = 15;
-                UIText.text = "Ik loop hier al rond zolang ik leef, mijn vader was ooit de oude directeur van deze plek." +
-                    " Dus je zou wel kunnen zeggen dat ik ben opgegroeid in dit theater." +
-                    " Ik ben hier altijd en ik ken alle hoeken en gaten van dit theater," +
-                    " maar toch kom je elke keer weer nieuwe dingen tegen";
-            }
-
-            if (!theater.isPlaying && firstTimeCasette)
-            {
-                firstTimeCasette = false;
-                casette.Play();
-                UIText.text = "Nu we het er toch over hebben, ik heb gisteren een casettebandje gevonden." +
-                    " Ik ben hem helaas kwijt geraakt maar ik denk dat ik wel weet waar hij ligt." +
-                    " Ik had gisteren namelijk een ander jasje aan, zou je kunnen kijken in de garderobe of hij in mijn rode colbert met gouden knopen ligt?";
-            }
-            if(!casette.isPlaying && !firstTimeCasette)
-            {
-                UI.SetActive(false);
-                Game.SetActive(true);
-            }
+            UIText.text = audioFragment.text;
+            UIText.fontSize = audioFragment.fontSize;
         }
+
+    void onDone()
+    {
+        UI.SetActive(false);
+        Game.SetActive(true);
+    }
+
+IEnumerator example(List<UIObject> objects, Action<UIObject> onCurrentAudioFragementChanged, Action onDone)
+    {
+        for (int index = 0; index < objects.Count; index++) {
+            var audioFragment = objects[index];
+            audioFragment.audioSource.Play();
+            onCurrentAudioFragementChanged(audioFragment);
+            yield return new WaitWhile(() => audioFragment.audioSource.isPlaying);
+        }
+
+        onDone();
     }
 }
