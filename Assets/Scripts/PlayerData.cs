6654343c993b0254a769b2e97c1bf3593d535d55
Gijs <gijswithagen@gmail.com>
2021-06-17 10:39:30 +0200

    readded player after merge

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : IData
+{
+    public int currentLevel;
+
+    public PlayerData(Player player) {
+        currentLevel = player.currentLevel;
+    }
+
+}
